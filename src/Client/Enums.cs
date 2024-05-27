579ca3c1afd12507d8f56dbf0ee8318eda3e9b8c
0cmenog <poupoumicou@gmail.com>
2024-05-27 06:40:25 -0700

    v2, compatible v2 SharpHoundCommon

---
+++ src/Client/Enums.cs
@@ -0,0 +1,33 @@
+﻿namespace Sharphound.Client
+{
+    /// <summary>
+    ///     Enums representing the possible collection methods specified in options
+    /// </summary>
+    public enum CollectionMethodOptions
+    {
+        None,
+        Group,
+        Session,
+        LoggedOn,
+        Trusts,
+        ACL,
+        ObjectProps,
+        RDP,
+        DCOM,
+        LocalAdmin,
+        PSRemote,
+        SPNTargets,
+        Container,
+        GPOLocalGroup,
+        LocalGroup,
+        UserRights,
+        Default,
+        DCOnly,
+        ComputerOnly,
+        CARegistry,
+        DCRegistry,
+        CertServices,
+        Misc,
+        All
+    }
+}
